ed52a46f9786b50500c79371c0a8cabde448301b
zabulyaka <zabulyaka@gmail.com>
2025-02-04 17:11:09 +0500

    Added hexlet_pytest app

---
+++ hexlet_pytest/hello.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from hexlet-pytest!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ hexlet_pytest/tests/test_example.py
@@ -0,0 +1,9 @@
+from hexlet_pytest.example import reverse
+
+
+def test_reverse():
+    assert reverse('Hexlet') == 'telxeH'
+
+
+def test_reverse_for_empty_string():
+    assert reverse('') == ''

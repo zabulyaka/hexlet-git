40df4eeccb29d331d8e3395ef1763ef443f228d5
zabulyaka <zabulyaka@gmail.com>
2025-02-08 11:38:54 +0500

    Added test_data, new test

--- hexlet_pytest/tests/test_example.py
+++ hexlet_pytest/tests/test_example.py
@@ -1,5 +1,22 @@
 from hexlet_pytest.example import reverse
 
+from pathlib import Path
+
+def get_test_data_path(filename):
+    return Path(__file__).parent / 'test_data' / filename
+
+def read_file(filename):
+    return get_test_data_path(filename).read_text()
+
+# тестируем функцию process(), которая как-то обрабатывает файл
+def test_reverse_with_data():
+    before = read_file('before.txt')
+    expected = read_file('result.txt')
+    actual = reverse(before.rstrip())
+
+    assert actual == expected.rstrip()
+
+
 
 def test_reverse():
     assert reverse('Hexlet') == 'telxeH'
